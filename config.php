112e76e763599ff23d30593f7a79eeb7c75c0827
Shubham <101355793+SJ0404@users.noreply.github.com>
2022-08-23 17:46:33 +0530

    Add files via upload
    Errors

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
+    'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+    'client_id' => 'ABDHsA3LlfKpwbQYqre8Y5TXv3U4OzaJ7abyYT8gW9dOaV2iFS',
+    'client_secret' => 'RZSK8uiJwrBLEitTlkgzrJTc7y2nTUoZCW0rerf6',
+    'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
+    'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
+)
+?>
